e0fdcce2f01fa32319bd647dbc40a2660ce4695d
vlad <vkuzir7@gmail.com>
2024-01-19 22:21:09 +0300

    Added functions for cancel quiz in any moment and add configuration file for Hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,23 +8,23 @@ import java.util.Properties;
 
 
 public class Main {
-  public static void main(String[] args) throws NullPointerException, IOException {
-    TelegramBot bot = new TelegramBot(loadToken());
-    QuizRepository readRepository = new QuizRepository("src/main/resources/quizes");
-    BotUpdate listener = new BotUpdate(bot, readRepository);
-    QuizBotExceptionHandler exception = new QuizBotExceptionHandler();
-    bot.setUpdatesListener(listener, exception);
-  }
-
-  private static String loadToken() throws IOException {
-    Properties prop = new Properties();
-    try (InputStream systemResourceAsStream = ClassLoader.getSystemResourceAsStream("config.properties")) {
-      prop.load(systemResourceAsStream);
-      String token = prop.getProperty("token");
-      if (token == null) {
-        throw new RuntimeException("Unable to load token");
-      }
-      return token;
+    public static void main(String[] args) throws NullPointerException, IOException {
+        TelegramBot bot = new TelegramBot(loadToken());
+        QuizRepository readRepository = new QuizRepository("src/main/resources/quizes");
+        BotUpdate listener = new BotUpdate(bot, readRepository);
+        QuizBotExceptionHandler exception = new QuizBotExceptionHandler();
+        bot.setUpdatesListener(listener, exception);
     }
-  }
+
+    private static String loadToken() throws IOException {
+        Properties prop = new Properties();
+        try (InputStream systemResourceAsStream = ClassLoader.getSystemResourceAsStream("config.properties")) {
+            prop.load(systemResourceAsStream);
+            String token = prop.getProperty("token");
+            if (token == null) {
+                throw new RuntimeException("Unable to load token");
+            }
+            return token;
+        }
+    }
 }
--- src/main/java/org/example/model/UserQuizSession.java
+++ src/main/java/org/example/model/UserQuizSession.java
@@ -5,57 +5,53 @@ import java.util.Iterator;
 
 public class UserQuizSession {
 
-  private final int questionAmount;
-  private final Iterator<Question> questionIterator;
-  private Question currentQuestion;
-  private int quizCounter = 0;
-  private int rightAnswerCounter = 0;
-  private boolean quizMode = true;
+    private final int questionAmount;
+    private final Iterator<Question> questionIterator;
+    private Question currentQuestion;
+    private int questionCounter = 0;
+    private int rightAnswerCounter = 0;
+    private boolean quizMode = true;
 
-  public UserQuizSession(Collection<Question> questions) {
-    this.questionIterator = questions.iterator();
-    this.questionAmount = questions.size();
+    public UserQuizSession(Collection<Question> questions) {
+        this.questionIterator = questions.iterator();
+        this.questionAmount = questions.size();
 //    this.currentQuestion = this.questionIterator.next();
-  }
+    }
 
-  public boolean isQuizMode() {
-    return quizMode;
-  }
+    public boolean isQuizMode() {
+        return quizMode;
+    }
 
-  public boolean isNextQuestionAvailable() {
-    return questionIterator.hasNext();
-  }
+    public boolean isNextQuestionAvailable() {
+        return questionIterator.hasNext();
+    }
 
-  public void addRightCounter() {
-    rightAnswerCounter++;
-  }
+    public void addRightCounter() {
+        rightAnswerCounter++;
+    }
 
-  public void addQuizCounter() {
-    quizCounter++;
-  }
+    public void setQuizMode(boolean quizMode) {
+        this.quizMode = quizMode;
+    }
 
-  public void setQuizMode(boolean quizMode) {
-    this.quizMode = quizMode;
-  }
+    public Question getCurrentQuestion() {
+        return currentQuestion;
+    }
 
-  public Question getCurrentQuestion() {
-    return currentQuestion;
-  }
+    public Question getNextQuestion() {
+        questionCounter++;
+        return currentQuestion = questionIterator.next();
+    }
 
-  public Question getNextQuestion() {
-    quizCounter++;
-    return currentQuestion = questionIterator.next();
-  }
+    public int getQuestionCounter() {
+        return questionCounter;
+    }
 
-  public int getQuizCounter() {
-    return quizCounter;
-  }
+    public int getQuestionAmount() {
+        return questionAmount;
+    }
 
-  public int getQuestionAmount() {
-    return questionAmount;
-  }
-
-  public int getRightAnswerCounter() {
-    return rightAnswerCounter;
-  }
+    public int getRightAnswerCounter() {
+        return rightAnswerCounter;
+    }
 }
